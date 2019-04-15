17b566b752b4d6be53558594d083a712351651eb
John <jroening@stumail.jccc.edu>
2019-04-15 14:12:20 -0500

    no message

---
+++ Doom/Assets/John/PlayerHealth.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [Tooltip("Player's max health.")]
+    int maxHealth = 100;
+    public int currentHealth = 100;
+
+    int maxArmor = 50;
+    public int currentArmor = 50;
+
+    void Awake()
+    {
+        currentHealth = maxHealth;
+        currentArmor = maxArmor;
+    }
+
+    void TakeDamage(int damage)
+    {
+        if (currentArmor > 0) //If armor is greater than 0.
+        {
+            if (damage > currentArmor) //If damage is greater than armor, deal damage to armor then health.
+            {
+                int totalDamage = damage - currentArmor; //Take damage minus the armor.
+                currentArmor = 0;
+
+                currentHealth -= totalDamage; //Deal remaining damage to the player.
+            }
+            else
+            {
+                currentArmor -= damage; //Else damage is greater than armor, deal damage to armor.
+            }
+        }
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+
+    }
+}
---
+++ Doom/Assets/John/PlayerMovement.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement: MonoBehaviour
+{
+    [Tooltip("How fast the player walks.")]
+    public float walkSpeed;
+    [Tooltip("How fast the player walks backwards.")]
+    public float reverseSpeed;
+    [Tooltip("How fast the player can move sideways.")]
+    public float strafeSpeed;
+    [Tooltip("How fast the player can rotate.")]
+    public float rotateSpeed;
+
+    //Total force of the player to send it in a direction.
+    Vector3 totalForce = Vector3.zero;
+
+    [Tooltip("How high the player can jump once.")]
+    public float jumpForce = 3f;
+    [Tooltip("Whether or not the player is on the ground.")]
+    [SerializeField] bool isGrounded = false;
+    [Tooltip("Whether or not the player has used double jump.")]
+    [SerializeField] bool doubleJump = false;
+    [Tooltip("The rigidbody of the player.")]
+    public Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        totalForce = Vector3.zero;
+        if (Input.GetAxis("Vertical") < 0)
+        {
+            totalForce += transform.forward * Input.GetAxis("Vertical") * (walkSpeed/2);
+        }
+        else
+        {
+            totalForce += transform.forward * Input.GetAxis("Vertical") * walkSpeed;
+        }
+        totalForce += transform.right * Input.GetAxis("Horizontal") * strafeSpeed;
+        transform.position += totalForce * Time.deltaTime;
+
+        transform.Rotate(new Vector3(0, Input.GetAxis("Mouse X"), 0) * Time.deltaTime * rotateSpeed);
+
+        isGrounded = Physics.Raycast(transform.position, Vector3.down, .1f);
+
+        if(Input.GetButtonDown("Jump") && ((isGrounded) || (doubleJump)))
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            if (isGrounded == false)
+            {
+                doubleJump = false;
+            }
+        }
+
+        if (isGrounded == true)
+        {
+            doubleJump = true;
+        }
+    }
+}
