85f05904ede74a0699c53d595ec7a0d8985040b8
VideoGameWarlord <videogamewarlord@gmail.com>
2019-04-16 22:23:10 -0500

    no message

---
+++ Doom/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+	private GameObject player;
+	private NavMeshAgent agent;
+
+    void Awake()
+    {
+		player = GameObject.FindGameObjectWithTag("Player");
+		agent = gameObject.GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		agent.SetDestination(player.transform.position);
+    }
+}
