433f72af1e59096b8edcacb1e40451a4aba9215c
John <jroening@stumail.jccc.edu>
2019-04-29 14:18:12 -0500

    UI

---
+++ Doom/Assets/John/UI_Health.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_Health : MonoBehaviour
+{
+    public Image healthBar;
+    public Image shieldBar;
+
+    public Text healthText;
+    public Text shieldText;
+
+    private PlayerHealth player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerHealth>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        healthBar.fillAmount = (float)player.currentHealth / player.maxHealth;
+        healthText.text = player.currentHealth.ToString();
+        shieldBar.fillAmount = (float)player.currentArmor / player.maxArmor;
+        shieldText.text = player.currentArmor.ToString();
+    }
+}
